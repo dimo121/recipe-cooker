c153f8eb4eefbe03888a60c55c1e776796495d1f
Dimo <autosales121@gmail.com>
2022-03-20 10:53:48 +1100

    webpack mod postcc

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,69 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 const isProduction = false;
 
 module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[hash:8].js',
-    sourceMapFilename: '[name].[hash:8].map',
-    chunkFilename: '[id].[hash:8].js',
-    publicPath: '/'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json', '.css'] 
-  },
-  target: 'web',
-  mode: isProduction ? 'production' : 'development', 
-  module: {
-    rules: [
-      {
-        test: /\.(tsx|ts)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true
-          }
-        }
-      }, 
-      { 
-        test: /\.s?css/,
-        use: [{
-          loader:'style-loader',
-          },{
-          loader:'css-loader',
-        },{
-          loader:'sass-loader',
-        },{
-          loader:'postcss-loader',
-          options: { // postcss plugins, can be exported to postcss.config.js
-            plugins: () => require('autoprefixer')
-          }}
-        ]
-      },
-      {
-        test: /\.(jpe?g|png|svg)$/,
-        use: ['file-loader'],
-      }, 
-    ]
-  }, 
-  plugins: [
-    new ForkTsCheckerWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Recipe Cooker',
-      template: './public/index.html',
-      filename: './index.html',
-      favicon: './public/favicon.ico'
-    })
-  ],
-  devtool: !isProduction ? 'source-map' : false
-}
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash:8].js',
+        sourceMapFilename: '[name].[hash:8].map',
+        chunkFilename: '[id].[hash:8].js',
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json', '.css'],
+    },
+    target: 'web',
+    mode: isProduction ? 'production' : 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(tsx|ts)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                    },
+                },
+            },
+            {
+                test: /\.s?css/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(jpe?g|png|svg)$/,
+                use: ['file-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Recipe Cooker',
+            template: './public/index.html',
+            filename: './index.html',
+            favicon: './public/favicon.ico',
+        }),
+    ],
+    devtool: !isProduction ? 'source-map' : false,
+};
+
+// {
+//   loader:'postcss-loader',
+//   options: { // postcss plugins, can be exported to postcss.config.js
+//     plugins: () => require('autoprefixer')
+//   }}
